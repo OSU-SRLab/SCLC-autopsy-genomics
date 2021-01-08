a4f270f192127e849c570c5efd6ec8d30b9b1324
rbonneville <russell.bonneville@osumc.edu>
2021-01-08 16:29:39 -0500

    download_resources.sh: remove unused gmap reference, create directories as needed

--- download_resources.sh
+++ download_resources.sh
@@ -1,12 +1,10 @@
-cd resources/reference_data/gmap
-wget ftp://ftp.ensembl.org/pub/grch37/current/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.dna.toplevel.fa.gz
-gunzip Homo_sapiens.GRCh37.dna.toplevel.fa.gz
-
-cd ../hisat2
+mkdir resources/reference_data/hisat2
+cd resources/reference_data/hisat2
 wget https://genome-idx.s3.amazonaws.com/hisat/grch37_snptran.tar.gz
 tar -xzf grch37_snptran.tar.gz
 rm grch37_snptran.tar.gz
 
+mkdir ../stringtie
 cd ../stringtie
 wget ftp://ftp.ensembl.org/pub/grch37/current/gtf/homo_sapiens/Homo_sapiens.GRCh37.87.gtf.gz
 gunzip Homo_sapiens.GRCh37.87.gtf.gz
